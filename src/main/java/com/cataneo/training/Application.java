e3cbb00a9701eeab6d894de12e6c62a0f7bca5af
Alexander Schwartz <alexander.Schwartz@cataneo.de>
2017-04-21 13:21:25 +0200

    Setting Java source in Pom file

--- src/main/java/com/cataneo/training/Application.java
+++ src/main/java/com/cataneo/training/Application.java
@@ -1,9 +1,17 @@
 package com.cataneo.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        System.out.println (numbers);
     }
 
     // method main(): ALWAYS the APPLICATION entry point
